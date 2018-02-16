06eb19f091816508717df5b1553e4857869a76be
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-02-16 09:06:54 -0800

    Initial setup for the project

--- WordCounter/Startup.cs
+++ WordCounter/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ProjectNameProject
+namespace WordCounterProject
 {
     public class Startup
     {
