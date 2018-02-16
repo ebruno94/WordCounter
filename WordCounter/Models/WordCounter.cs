a91d96c9a209080b514615c37dafb16e55ec9488
Ernest Bruno <ernest.bruno94@yahoo.com>
2018-02-16 09:56:37 -0800

    Create initial logic based on test results

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -8,9 +8,31 @@ namespace WordCounterProject
     {
         private Dictionary<string, int> _dictionary = new Dictionary<string,int>(){};
 
+        // SIMPLE CASE
         public int CountWords(string word)
         {
             return word.Split(' ').Length;
+        }
+
+        public void SetDictionary(string input)
+        {
+            string[] wordArray = input.Split(' ');
+            foreach (string word in wordArray)
+            {
+                if (!_dictionary.ContainsKey(word))
+                {
+                    _dictionary.Add(word, 1);
+                }
+                else
+                {
+                    _dictionary[word]++;
+                }
+            }
+        }
+
+        public Dictionary<string, int> GetDictionary()
+        {
+            return _dictionary;
         }
     }
 }
